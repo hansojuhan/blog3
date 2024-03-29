35f2680d91f6f8066906001c3dcc0937f2661e91
hje <hjerikson@gmail.com>
2024-03-29 11:47:49 +0100

    db:seed templates to generate fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# (1..5).each do |id|
+#   User.create!(
+# # each user is assigned an id from 1-20
+#       id: id+1, 
+#       email: Faker::Internet.email,
+# # issue each user the same password
+#       password: "password", 
+#       password_confirmation: "password",
+#   )
+# # end
+
+# (1..20).each do |id|
+#   Post.create!(
+#       id: rand(100..200),
+#       user_id: rand(2..6), 
+#       title: Faker::Hipster.sentences.sample,
+#       body: Faker::Lorem.paragraphs 
+#   )
+# end
